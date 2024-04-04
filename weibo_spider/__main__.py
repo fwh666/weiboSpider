912441f73af6e088de7b506cca98a327cc986bac
fwh-mac-mini <fuwenhao594@163.com>
2024-04-04 15:01:57 +0800

    add: 定时采集

--- weibo_spider/__main__.py
+++ weibo_spider/__main__.py
@@ -1,8 +1,21 @@
 import os
 import sys
-
+import schedule
+import time
 from absl import app
 sys.path.append(os.path.abspath(os.path.dirname(os.getcwd())))
 from weibo_spider.spider import main
+def schedule_job():
+    # schedule.every(10).minutes.do(job)
+    schedule.every(1).hours.do(job)
+    # schedule.every(1).seconds.do(job)
 
-app.run(main)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+def job():
+    # print("开始采集")
+    app.run(main)
+print('[程序启动...微博每一小时采集]')
+schedule_job()
+# app.run(main)
